7a4ca8cbf965b0784ee38adb92dc90b4af31874c
nisatas <nisatas544@gmail.com>
2025-12-16 22:37:53 +0300

    add project url to index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function cypressTestProjesi() {
+  const checklist = {
+    project_url: "https://github.com/nisatas/cypress-login-project",
+  };
+
+  return checklist;
+}
+
+/* Bu satırdan sonraki satırları silmeyin! */
+module.exports = cypressTestProjesi;
